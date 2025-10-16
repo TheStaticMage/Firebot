659d64c11f82bef9dc7a3ff82b89922313f9149f
The Static Mage <199814801+TheStaticMage@users.noreply.github.com>
2025-10-16 03:13:23 -0500

    Move message hide filter further up the chain

--- src/gui/app/app-main.js
+++ src/gui/app/app-main.js
@@ -606,6 +606,18 @@
         };
     });
 
+    app.filter("hideHiddenMessages", function() {
+        return function(elements) {
+            return elements.filter((e) => {
+                if (e.type !== 'message') {
+                    return true;
+                }
+                return e.data.isHiddenFromChatFeed !== true;
+            }
+            );
+        };
+    });
+
     app.filter("hideRewardRedemptions", function(settingsService) {
         return function(elements) {
             const shouldHide = settingsService.getSetting("ChatHideRewardRedemptions");
